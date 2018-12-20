b88226ca49af57aee8087a48c33795500a4c2beb
Evan Yang <evan.yang@schindler.com>
2018-12-20 22:00:27 +0800

    Create a empty about screen.

---
+++ LifeSchedule/LifeSchedule/Setting/AboutScreen/AboutUsScreenViewController.h
@@ -0,0 +1,17 @@
+//
+//  AboutUsScreenViewController.h
+//  LifeSchedule
+//
+//  Created by Evan Yang on 2018/12/3.
+//  Copyright © 2018 EvanYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AboutUsScreenViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LifeSchedule/LifeSchedule/Setting/AboutScreen/AboutUsScreenViewController.m
@@ -0,0 +1,32 @@
+//
+//  AboutUsScreenViewController.m
+//  LifeSchedule
+//
+//  Created by Evan Yang on 2018/12/3.
+//  Copyright © 2018 EvanYang. All rights reserved.
+//
+
+#import "AboutUsScreenViewController.h"
+
+@interface AboutUsScreenViewController ()
+
+@end
+
+@implementation AboutUsScreenViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
