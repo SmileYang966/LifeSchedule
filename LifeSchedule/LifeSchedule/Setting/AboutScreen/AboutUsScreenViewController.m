bbb00fd2d09bb8d1c6692b1384e44184b40c3925
Evan Yang <evanyang966@163.com>
2019-03-15 12:51:47 +0800

    LS22.1-Modify the font name on the About screen

--- LifeSchedule/LifeSchedule/Setting/AboutScreen/AboutUsScreenViewController.m
+++ LifeSchedule/LifeSchedule/Setting/AboutScreen/AboutUsScreenViewController.m
@@ -27,12 +27,20 @@
 -(UIView *)iconView{
     if (_iconView == NULL) {
         _iconView = [[UIView alloc]initWithFrame:CGRectMake(0,0,self.view.bounds.size.width,200)];
+        _iconView.backgroundColor = UIColor.clearColor;
         UIImage *img = [UIImage imageNamed:@"list.png"];
-        UIImageView *imgView = [[UIImageView alloc]initWithFrame:CGRectMake((self.view.bounds.size.width-img.size.width)*0.5f, (_iconView.bounds.size.height-img.size.height-64)*0.5 + 64 , img.size.width, img.size.height)];
-//        imgView.backgroundColor = UIColor.redColor;
+        UIImageView *imgView = [[UIImageView alloc]initWithFrame:CGRectMake((self.view.bounds.size.width-img.size.width)*0.5f, (_iconView.bounds.size.height-img.size.height-64)*0.5 + 54 , img.size.width, img.size.height)];
         imgView.image = img;
         [_iconView addSubview:imgView];
+        
+        UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(imgView.frame.origin.x-3, CGRectGetMaxY(imgView.frame)-10.0f, imgView.frame.size.width+20, 30.0f)];
+//        label.font = [UIFont fontWithName:@"Cloudtype-JGjingxinkaiGB-Regular" size:23.0f];
+        label.font = [UIFont fontWithName:@"IowanOldStyle-BoldItalic" size:21.0f];
+        label.textColor = [UIColor colorWithRed:66/255.0 green:130/255.0 blue:210/255.0 alpha:1.0f];
+        label.text = @"LifeSchedule";
+        
         [self.view addSubview:_iconView];
+        [self.view addSubview:label];
     }
     return _iconView;
 }
@@ -52,10 +60,20 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     [self aboutTableView];
+    
+    /*Check the all supported fonts
+    for (NSString *fontFamilyName in [UIFont familyNames]) {
+        NSLog(@"fontFamilyName=%@",fontFamilyName);
+        for (NSString *fontName in [UIFont fontNamesForFamilyName:fontFamilyName]) {
+            NSLog(@"font:%@",fontName);
+        }
+        NSLog(@"-------------\n\n");
+    }
+    */
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 2;
+    return 3;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -69,8 +87,11 @@
             cell.textLabel.text = @"版本";
             cell.detailTextLabel.text = @"V0.01";
             break;
-          
         case 1:
+            cell.textLabel.text = @"作者";
+            cell.detailTextLabel.text = @"Evan Yang";
+            break;
+        case 2:
             cell.textLabel.text = @"邮箱";
             cell.detailTextLabel.text = @"evanyang966@163.com";
             break;
