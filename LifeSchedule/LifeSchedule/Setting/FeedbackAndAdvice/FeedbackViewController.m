e52f7298106b15e22c93a81c9c13aae21e183d4a
Evan Yang <evanyang966@163.com>
2019-03-16 13:53:27 +0800

    LS23.2-Create the MBProgressHUD view and show it on the screen.

--- LifeSchedule/LifeSchedule/Setting/FeedbackAndAdvice/FeedbackViewController.m
+++ LifeSchedule/LifeSchedule/Setting/FeedbackAndAdvice/FeedbackViewController.m
@@ -9,11 +9,14 @@
 #import "FeedbackViewController.h"
 #import "SKPSMTPMessage.h"
 #import "NSData+Base64Additions.h"
+#import "MBProgressHUD.h"
 
 @interface FeedbackViewController ()<SKPSMTPMessageDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *subjectTf;
 @property (weak, nonatomic) IBOutlet UITextView *contentTv;
+
+@property(nonatomic,strong) MBProgressHUD *progressHUD;
 
 @end
 
@@ -32,12 +35,18 @@
 }
 
 -(void)initOperations{
-    //Content TextView
+    //1. Content TextView
     self.contentTv.layer.backgroundColor = UIColor.clearColor.CGColor;
     self.contentTv.layer.borderColor = [UIColor colorWithRed:226/255.0 green:226/255.0 blue:226/255.0 alpha:1.0f].CGColor;
     self.contentTv.layer.borderWidth = 1.0f;
     self.contentTv.layer.cornerRadius = 8.0f;
     [self.contentTv.layer setMasksToBounds:YES];
+    
+    //2. Create a MBProgressHUD view
+    self.progressHUD = [[MBProgressHUD alloc]initWithView:self.view];
+    self.progressHUD.progress = 0.4f;
+    self.progressHUD.dimBackground = YES;
+    [self.view addSubview:self.progressHUD];
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
@@ -45,18 +54,21 @@
 }
 
 -(void)sendButtonClicked:(UIBarButtonItem *)barButtonItem{
+    //开始显示遮罩
+     [self.progressHUD showAnimated:YES];
+    
     //1. Collect subject info
     NSString *subjectInfo = self.subjectTf.text;
-    
+
     //2. Collect content info
     NSString *contentTv = self.contentTv.text;
-    
+
     //3. send mail from addr
     NSString *sendAddr = @"lifeschedule@163.com";
-    
+
     //4. mail to addr
     NSString *toAddr = @"evanyang966@163.com";
-    
+
     SKPSMTPMessage *sendMessage = [[SKPSMTPMessage alloc]init];
     sendMessage.fromEmail = sendAddr;
     sendMessage.toEmail = toAddr;
@@ -67,10 +79,10 @@
     sendMessage.subject = subjectInfo;
     sendMessage.wantsSecure = YES;
     sendMessage.delegate = self;
-    
+
     NSDictionary *plainPart = [NSDictionary dictionaryWithObjectsAndKeys:@"text/plain",kSKPSMTPPartContentTypeKey,[NSString stringWithFormat:@"%@",contentTv]
                             ,kSKPSMTPPartMessageKey,@"8bit",kSKPSMTPPartContentTransferEncodingKey,nil];
-    
+
     sendMessage.parts = [NSArray arrayWithObjects:plainPart, nil];
     [sendMessage send];
 }
@@ -86,6 +98,9 @@
 }
 
 - (void)showPopUpWithTitle:(NSString *)title message:(NSString *)msg{
+    //隐藏遮罩
+    [self.progressHUD hideAnimated:YES];
+    
     UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title message:msg preferredStyle:UIAlertControllerStyleAlert];
     [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
         [self.navigationController popViewControllerAnimated:YES];
