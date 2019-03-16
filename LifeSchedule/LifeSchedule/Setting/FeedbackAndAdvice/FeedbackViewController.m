d12d06a054379af4ec36bbe68d7e96ac84c2a7a3
Evan Yang <evanyang966@163.com>
2019-03-16 11:26:33 +0800

    LS23.1-Finished the basic functions for the feedback function, it need to do the future enhancement

--- LifeSchedule/LifeSchedule/Setting/FeedbackAndAdvice/FeedbackViewController.m
+++ LifeSchedule/LifeSchedule/Setting/FeedbackAndAdvice/FeedbackViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "FeedbackViewController.h"
+#import "SKPSMTPMessage.h"
+#import "NSData+Base64Additions.h"
 
-@interface FeedbackViewController ()
+@interface FeedbackViewController ()<SKPSMTPMessageDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *subjectTf;
 @property (weak, nonatomic) IBOutlet UITextView *contentTv;
@@ -21,6 +23,10 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
+    //1.Initalization
+    [self initOperations];
+    
+    //2.Clicked the send button
     UIBarButtonItem *sendBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"Send" style:UIBarButtonItemStylePlain target:self action:@selector(sendButtonClicked:)];
     self.navigationItem.rightBarButtonItem = sendBarButtonItem;
 }
@@ -34,8 +40,58 @@
     [self.contentTv.layer setMasksToBounds:YES];
 }
 
--(void)sendButtonClicked:(UIBarButtonItem *)barButtonItem{
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+}
 
+-(void)sendButtonClicked:(UIBarButtonItem *)barButtonItem{
+    //1. Collect subject info
+    NSString *subjectInfo = self.subjectTf.text;
+    
+    //2. Collect content info
+    NSString *contentTv = self.contentTv.text;
+    
+    //3. send mail from addr
+    NSString *sendAddr = @"lifeschedule@163.com";
+    
+    //4. mail to addr
+    NSString *toAddr = @"evanyang966@163.com";
+    
+    SKPSMTPMessage *sendMessage = [[SKPSMTPMessage alloc]init];
+    sendMessage.fromEmail = sendAddr;
+    sendMessage.toEmail = toAddr;
+    sendMessage.relayHost = @"smtp.163.com";
+    sendMessage.requiresAuth = YES;
+    sendMessage.login = sendAddr;
+    sendMessage.pass = @"lifeschedule966";
+    sendMessage.subject = subjectInfo;
+    sendMessage.wantsSecure = YES;
+    sendMessage.delegate = self;
+    
+    NSDictionary *plainPart = [NSDictionary dictionaryWithObjectsAndKeys:@"text/plain",kSKPSMTPPartContentTypeKey,[NSString stringWithFormat:@"%@",contentTv]
+                            ,kSKPSMTPPartMessageKey,@"8bit",kSKPSMTPPartContentTransferEncodingKey,nil];
+    
+    sendMessage.parts = [NSArray arrayWithObjects:plainPart, nil];
+    [sendMessage send];
+}
+
+- (void)messageSent:(SKPSMTPMessage *)message{
+    NSLog(@"messageSuccessed-----%@", message);
+    [self showPopUpWithTitle:@"提示信息" message:@"发送成功!"];
+}
+
+- (void)messageFailed:(SKPSMTPMessage *)message error:(NSError *)error{
+    NSLog(@"messageFailedToSend-----%@", message);
+    [self showPopUpWithTitle:@"提示信息" message:@"发送失败，请稍后再试!"];
+}
+
+- (void)showPopUpWithTitle:(NSString *)title message:(NSString *)msg{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title message:msg preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [self.navigationController popViewControllerAnimated:YES];
+    }]];
+    
+    [self.navigationController presentViewController:alertController animated:YES completion:nil];
 }
 
 @end
