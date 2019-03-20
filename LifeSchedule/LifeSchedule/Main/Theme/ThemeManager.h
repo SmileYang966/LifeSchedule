bfab360201b0ae17c564c0c31549d1d93e2c1673
Evan Yang <evanyang966@163.com>
2019-03-20 13:51:37 +0800

    LS26.1-Create a ThemeManager

---
+++ LifeSchedule/LifeSchedule/Main/Theme/ThemeManager.h
@@ -0,0 +1,17 @@
+//
+//  ThemeManager.h
+//  LifeSchedule
+//
+//  Created by Evan Yang on 2019/3/20.
+//  Copyright © 2019 EvanYang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ThemeManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LifeSchedule/LifeSchedule/Main/Theme/ThemeManager.m
@@ -0,0 +1,13 @@
+//
+//  ThemeManager.m
+//  LifeSchedule
+//
+//  Created by Evan Yang on 2019/3/20.
+//  Copyright © 2019 EvanYang. All rights reserved.
+//
+
+#import "ThemeManager.h"
+
+@implementation ThemeManager
+
+@end
