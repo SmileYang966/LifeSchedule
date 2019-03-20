fef048893d132574d569b16bc9dabf26c5d191b2
Evan Yang <evanyang966@163.com>
2019-03-20 17:39:44 +0800

    LS26.5-Create the collectionView to provide the possibility that user can select the theme by themselves

--- LifeSchedule/LifeSchedule/Setting/Preference/Controllers/LSThemeStyleViewController.m
+++ LifeSchedule/LifeSchedule/Setting/Preference/Controllers/LSThemeStyleViewController.m
@@ -8,26 +8,57 @@
 
 #import "LSThemeStyleViewController.h"
 
-@interface LSThemeStyleViewController ()
+@interface LSThemeStyleViewController ()<UICollectionViewDelegate,UICollectionViewDataSource>
+
+@property(nonatomic,strong) UICollectionView *collectionView;
 
 @end
 
+static NSString *collectionCellReuseId = @"collectionCellID";
+CGFloat cellMargin = 10.0f;
+
 @implementation LSThemeStyleViewController
+
+- (UICollectionView *)collectionView{
+    if (_collectionView == NULL) {
+        UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc]init];
+        CGFloat itemWidth = (self.view.bounds.size.width - 4 * cellMargin) / 3.0f;
+        CGFloat itemHeight = 150.0f;
+        flowLayout.itemSize = CGSizeMake(itemWidth, itemHeight);
+        
+        CGFloat collectionViewX = cellMargin;
+        CGFloat collectionViewY = cellMargin;
+        CGFloat collectionViewWidth = self.view.bounds.size.width - 2*collectionViewX;
+        
+        _collectionView = [[UICollectionView alloc]initWithFrame:CGRectMake(collectionViewX, collectionViewY+64, collectionViewWidth, self.view.bounds.size.height) collectionViewLayout:flowLayout];
+        [_collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:collectionCellReuseId];
+        _collectionView.dataSource = self;
+        _collectionView.delegate = self;
+        [self.view addSubview:_collectionView];
+    }
+    return _collectionView;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.view.backgroundColor = UIColor.brownColor;
+    self.view.backgroundColor = UIColor.whiteColor;
+    self.collectionView.backgroundColor = UIColor.whiteColor;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return 5;
 }
-*/
+
+// The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:collectionCellReuseId forIndexPath:indexPath];
+    cell.backgroundColor = UIColor.redColor;
+    return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    int a = 10;
+}
 
 @end
