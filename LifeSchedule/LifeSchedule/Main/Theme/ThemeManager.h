162483c48e88872734e803254823b8da2bb6cf29
Evan Yang <evanyang966@163.com>
2019-03-20 15:00:02 +0800

    LS26.2-Create the ThemeManager

--- LifeSchedule/LifeSchedule/Main/Theme/ThemeManager.h
+++ LifeSchedule/LifeSchedule/Main/Theme/ThemeManager.h
@@ -7,10 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ThemeManager : NSObject
+
+@property (nonatomic, copy) NSString *themeName;           // 主题名字
+@property (nonatomic, retain) NSDictionary *themePlistDict;    // 主题属性列表字典
+
++ (ThemeManager *) sharedThemeManager;
+
+- (UIImage *) themeImageWithName:(NSString *)imageName;
 
 @end
 
--- LifeSchedule/LifeSchedule/Main/Theme/ThemeManager.m
+++ LifeSchedule/LifeSchedule/Main/Theme/ThemeManager.m
@@ -8,6 +8,61 @@
 
 #import "ThemeManager.h"
 
+static ThemeManager *_sharedThemeManager;
+
+@interface  ThemeManager()
+
+@end
+
 @implementation ThemeManager
+
+- (instancetype) init {
+    if(self = [super init]) {
+        NSString *themePath = [[NSBundle mainBundle] pathForResource:@"theme" ofType:@"plist"];
+        self.themePlistDict = [NSDictionary dictionaryWithContentsOfFile:themePath];
+    }
+    return self;
+}
+
+
++ (ThemeManager *) sharedThemeManager {
+    @synchronized(self) {
+        if (_sharedThemeManager == NULL) {
+            _sharedThemeManager = [[ThemeManager alloc]init];
+        }
+    }
+    return _sharedThemeManager;
+}
+
+- (void)setThemeName:(NSString *)themeName{
+    _themeName = themeName;
+}
+
+
+- (UIImage *) themeImageWithName:(NSString *)imageName {
+    if (imageName == nil) {
+        return nil;
+    }
+    
+    NSString *themePath = [self themePath];
+    NSString *themeImagePath = [themePath stringByAppendingPathComponent:imageName];
+    UIImage *themeImage = [UIImage imageWithContentsOfFile:themeImagePath];
+    
+    return themeImage;
+}
+
+// 返回主题路径
+- (NSString *)themePath {
+    NSString *resourcePath = [[NSBundle mainBundle] resourcePath];
+    if (self.themeName == nil || [self.themeName isEqualToString:@""]) {
+        return resourcePath;
+    }
+    
+    
+    NSString *themeSubPath = [self.themePlistDict objectForKey:self.themeName];    // Skins/blue
+    NSString *themeFilePath = [resourcePath stringByAppendingPathComponent:themeSubPath]; // .../Skins/blue
+    
+    return themeFilePath;
+}
 
 @end
