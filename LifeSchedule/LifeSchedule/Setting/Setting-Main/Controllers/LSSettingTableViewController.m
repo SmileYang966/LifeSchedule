b0bb897ea6cb6cb5a1f558cf3158752623e79fef
Evan Yang <evanyang966@163.com>
2019-04-25 17:01:47 +0800

    LS46.1-We just found use the "SKPSMTPMessage" to send the mail , there're many issues existed in that lib and no update for a long time.To keep the app in a steady status , we just remove the feedback and advice screen tempoarily.

--- LifeSchedule/LifeSchedule/Setting/Setting-Main/Controllers/LSSettingTableViewController.m
+++ LifeSchedule/LifeSchedule/Setting/Setting-Main/Controllers/LSSettingTableViewController.m
@@ -32,20 +32,22 @@
         LSSettingSectionModel *section0 = [[LSSettingSectionModel alloc]init];
         LSSettingCellModel *themesModel = [LSArrowSettingCellModel LSSettingCellModelWithTitle:@"配置" iconWithName:@"config" destClass:[ConfigurationViewController class]];
         LSSettingCellModel *advancedOptionsModel = [LSArrowSettingCellModel LSSettingCellModelWithTitle:@"偏好设置" iconWithName:@"preference" destClass:[LSSettingPreferenceViewController class]];
-        NSArray *group0 =  @[themesModel,advancedOptionsModel];
+        LSSettingCellModel *aboutModel = [LSArrowSettingCellModel LSSettingCellModelWithTitle:@"关于" iconWithName:@"about" destClass:[AboutUsScreenViewController class]];
+        NSArray *group0 =  @[themesModel,advancedOptionsModel,aboutModel];
         section0.sectionItems = group0;
         
         //section 2
+        /*
         LSSettingSectionModel *section2 = [[LSSettingSectionModel alloc]init];
-//        LSSettingCellModel *helpCenterModel = [LSArrowSettingCellModel LSSettingCellModelWithTitle:@"帮助中心" iconWithName:@"MoreHelp" destClass:[LSSettingPreferenceViewController class]];
+        LSSettingCellModel *helpCenterModel = [LSArrowSettingCellModel LSSettingCellModelWithTitle:@"帮助中心" iconWithName:@"MoreHelp" destClass:[LSSettingPreferenceViewController class]];
         LSSettingCellModel *feedbackAndAdviceModel = [LSArrowSettingCellModel LSSettingCellModelWithTitle:@"反馈与建议" iconWithName:@"feedback" destClass:[FeedbackViewController class]];
-        LSSettingCellModel *aboutModel = [LSArrowSettingCellModel LSSettingCellModelWithTitle:@"关于" iconWithName:@"about" destClass:[AboutUsScreenViewController class]];
-//        LSSettingCellModel *shareModel = [LSArrowSettingCellModel LSSettingCellModelWithTitle:@"推荐给好友" iconWithName:@"MoreShare" destClass:[LSSettingPreferenceViewController class]];
-        NSArray *group2 = @[feedbackAndAdviceModel,aboutModel];
+        LSSettingCellModel *shareModel = [LSArrowSettingCellModel LSSettingCellModelWithTitle:@"推荐给好友" iconWithName:@"MoreShare" destClass:[LSSettingPreferenceViewController class]];
+        NSArray *group2 = @[feedbackAndAdviceModel];
         section2.sectionItems = group2;
+         */
         
         //添加group0、group1以及group2到这个dataList数组中去
-        _settingItemsList = @[section0,section2];
+        _settingItemsList = @[section0];
     }
     return _settingItemsList;
 }
