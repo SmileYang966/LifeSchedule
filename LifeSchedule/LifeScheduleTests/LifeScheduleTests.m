7ade682a9a9a46f2f5105512a194b7ce5345a1b7
Evan Yang <evanyang966@163.com>
2019-04-29 16:06:33 +0800

    LS48.1-Import the Unit Test and UI Test

--- LifeSchedule/LifeScheduleTests/LifeScheduleTests.m
+++ LifeSchedule/LifeScheduleTests/LifeScheduleTests.m
@@ -27,6 +27,8 @@
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    int a = 0;
+    XCTAssertTrue(a==0,@"a必须为0");
 }
 
 - (void)testPerformanceExample {
